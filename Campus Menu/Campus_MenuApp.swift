e6051953ec6c1ed86d28bebd8e37cdca4f6a808f
caganshbz17 <cagan.sahbaz17@gmail.com>
2025-12-06 15:41:32 +0300

    İOS

---
+++ Campus Menu/Campus_MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  Campus_MenuApp.swift
+//  Campus Menu
+//
+//  Created by Çağan Şahbaz on 6.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct Campus_MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Campus MenuTests/Campus_MenuTests.swift
@@ -0,0 +1,17 @@
+//
+//  Campus_MenuTests.swift
+//  Campus MenuTests
+//
+//  Created by Çağan Şahbaz on 6.12.2025.
+//
+
+import Testing
+@testable import Campus_Menu
+
+struct Campus_MenuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
